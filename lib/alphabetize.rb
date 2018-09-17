dd9ae1150b00bca08e5d0ec16b39c20a014cf9a0
Nicholas Smith <nico.ehs05@gmail.com>
2018-09-17 19:33:18 +0000

    automatically backed up by learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,6 +4,8 @@ def alphabetize(arr)
   arr.sort_by{|el| elword.gsub("c",'c0').gsub("g",'g0').gsub("h",'h0').gsub("j",'j0').gsub("s",'s0').gsub("u",'u0').gsub("ĉ",'c1').gsub("ĝ",'g1').gsub("ĥ",'h1').gsub("ĵ",'j1').gsub("ŝ",'s1').gsub("ŭ",'u1')}
 end
 
+[["c",'c0']["g",'g0']].map_with_index{|el| }[]
+
 
 def convert_1(word)
   word.gsub("c",'c0').gsub("g",'g0').gsub("h",'h0').gsub("j",'j0').gsub("s",'s0').gsub("u",'u0').gsub("ĉ",'c1').gsub("ĝ",'g1').gsub("ĥ",'h1').gsub("ĵ",'j1').gsub("ŝ",'s1').gsub("ŭ",'u1')
